cc8c1868e62407f886a0914451c62d926deb877e
xiansheng123 <732824600@qq.com>
2019-09-15 23:32:21 +0800

    add proxy

---
+++ WebProxy/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AspNetCore.Proxy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebProxy.Controllers
+{
+    public class ValuesController : ControllerBase
+    {
+        [Route("api/get/posts/{postId}")]
+        public Task Get(string postId)
+        {
+//            var options = ProxyOptions.Instance
+//                .WithShouldAddForwardedHeaders(false);
+            return this.ProxyAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
+        }
+        
+        [Route("api/post/posts")]
+        [HttpPost]
+        public Task Post(string str)
+        {
+//            var options = ProxyOptions.Instance
+//                .WithShouldAddForwardedHeaders(false);
+//            var aa = new StreamReader(this.Request.Body).ReadToEnd();
+            return this.ProxyAsync($"https://jsonplaceholder.typicode.com/posts");
+        }
+    }
+}
